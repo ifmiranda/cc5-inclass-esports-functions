830769ad51652cb22595f7bf2f8856e7e7a2edbd
Isabella Miranda <ifmiranda@usf.edu>
2025-09-24 17:19:11 -0400

    first commit

---
+++ ic_5.js
@@ -0,0 +1,18 @@
+// In-class coding challenge Esports Functions 
+
+const products = [ 
+    {id: 301, name: "Robot mouse", category:"gear", price: 25.59}, 
+    {id: 302, name: "Team wrist band", category: "apparel", price:10.99}, 
+    {id: 303, name: "Oreo protein bar", category: "snacks", price: 5.99},
+    {id: 304, name: "VIP lounge pass", category: "access", price: 12.99},
+    {id: 305, name: "SD card", category: "gear", price: 18.98}
+]; 
+
+// Customer cart 
+const cart = [ 
+    {productID: 302, qty: 1}, 
+    {productID: 303, qty: 5}, 
+    {productID: 304, qty: 3}, 
+    {productID: 305, qty: 4}, 
+]; 
+
